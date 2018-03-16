7ca9f546fb30ffd704614937c9d86d934cc818ac
Mattgit123 <35824137+Mattgit123@users.noreply.github.com>
2018-03-16 12:53:17 -0400

    Add files via upload

---
+++ About_Us/js/main.js
@@ -0,0 +1,24 @@
+/* Name: Matt Tounkara
+Date: 03/06/2018
+Description: This is my final project for Web Development 100 @ NYCDA in Philadelphia.
+             the aim of this project is to create an e-commerce shoe store website.
+             A website where you can shop for the finest and most elegant shoes you
+             could ever come across. A one stop shopping place for shoes. This is
+             the main.js for the About Us page. Its purpose is to apply javascript
+             and jQuery to the index.html of the About Us page. */
+
+
+$(document).ready(function() {
+
+
+        $("#theMessage").animate({right: '200px'}, "slow");
+        $("#theMessage").fadeOut(2000);
+        $("#theMessage").fadeIn(2000);
+        $("#theMessage").animate({left: '200px'}, "slow");
+        $("#theMessage").fadeOut(2000);
+        $("#theMessage").fadeIn(2000);
+        $("#theMessage").animate({left: '-10px'}, "slow");
+        $("#theMessage").fadeOut(2000);
+        $("#theMessage").fadeIn(2000);
+
+});
